5d8e392db0880a0d07e426e711cd89e08749cdcd
Misara <41232116+misaraadel@users.noreply.github.com>
2024-12-21 13:23:58 +0200

    edit data

--- _nuxt/static/1734731605/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Far\u002Fabout-us","\u002Fprojects","\u002Far\u002Fblogs","\u002Far","\u002F","\u002Far\u002Fprojects","\u002Fblogs","\u002Fproducts","\u002Fabout-us","\u002Fcontact","\u002Far\u002Fproducts","\u002Far\u002Fcontact"]})
