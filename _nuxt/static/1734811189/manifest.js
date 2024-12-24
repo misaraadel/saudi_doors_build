ad7e1421065e0fadb078b0d124504277a926d9cb
Misara <41232116+misaraadel@users.noreply.github.com>
2024-12-24 23:46:51 +0200

    add layout

--- _nuxt/static/1734811189/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Far\u002Fblogs","\u002F","\u002Far\u002Fcontact","\u002Fproducts","\u002Fblogs","\u002Fprojects","\u002Far\u002Fproducts","\u002Far\u002Fprojects","\u002Fcontact","\u002Far","\u002Fabout-us","\u002Far\u002Fabout-us"]})
